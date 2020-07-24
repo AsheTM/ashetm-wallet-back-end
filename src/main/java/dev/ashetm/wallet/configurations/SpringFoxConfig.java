f9084f1bb7cde218f92096b887633e6c4318c2db
AsheTM <haithemalidaoud@gmail.com>
2020-07-24 21:07:39 +0100

    Resolve issue of Swagger

--- src/main/java/dev/ashetm/wallet/configurations/SpringFoxConfig.java
+++ src/main/java/dev/ashetm/wallet/configurations/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package dev.ashetm.wallet.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +22,20 @@ public class SpringFoxConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("dev.ashetm.wallet"))
 				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.apiInfo(metadata());
+	}
+
+	private ApiInfo metadata() {
+		return new ApiInfoBuilder()
+				.title("AsheTM Wallet Back End API")
+				.contact(contact())
 				.build();
+	}
+
+	private Contact contact() {
+		return new Contact("DAWOUD Haitham", "", "contact@ashetm.com");
 	}
 
 }
